0d355c03f86fd52190951dbbbfa1879c784098f1
hankangwen <13538898378@163.com>
2023-12-29 09:55:44 +0800

    C#封装成DLL，并在C#中调用

---
+++ CallCSharpDLL/CallCSharpDLL/Form1.cs
@@ -0,0 +1,28 @@
+﻿using CreateCSharpDLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CallCSharpDLL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 class1 = new Class1();
+            int res = class1.add(1, 2);
+            Console.WriteLine("res = " + res);
+        }
+    }
+}
---
+++ CreateCSharpDLL/CreateCSharpDLL/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateCSharpDLL
+{
+    public class Class1
+    {
+        //封装的DLL函数
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
